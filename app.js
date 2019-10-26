c86c744a93ff838af43636b191e31dfa83532c7b
lim5max <lionmaks2017@yandex.ru>
2019-10-26 20:53:29 +0300

    rijwer

--- app.js
+++ app.js
@@ -1,72 +1,30 @@
-const path = require('path')
 let app = require('express')();
-let express = require('express');
 let http = require('http').createServer(app);
 let io = require('socket.io')(http)
-const layout = require('express-layout')
-const bodyParser = require("body-parser")
-//app.use(express.session());
-let student_array = [
+const bodyParser = require('body-parser')
+const session = require('express-session')
 
-{
-  login: "Tjaaa",
-  username: "Alexandr101"
-}
 
-]
-let teachers_array = [
-
-{
-  login: "Goka"
-}
-
+const midllewares = [
+  bodyParser.urlencoded()
 ]
 
-const middleware = [
-  layout(),
-  express.static(path.join(__dirname, 'templates')),
-]
-app.use(middleware)
+app.use(midllewares)
 
-
-app.use(bodyParser.urlencoded({extended: true}))
-
-app.get('/', function(req, res) {
+app.get('/dashboard', function(req, res) {
     
-    res.render('index');
+    res.sendFile(__dirname + '/templates/dashboard.html');
 
   })
-app.get('/login', function(req, res) {
-    
-  res.sendFile(__dirname + '/templates/login.html');
+
+app.post('/dashboard', function(req, res){
+  teacher_name  =  req.body.teachername
+  password      =  req.body.password
+
 
 })
 
-app.get('/classes', function(req, res) {
-    
-  res.sendFile(__dirname + '/templates/classes.html');
 
+http.listen(6700, ()=>{
+    console.log('listening on *:6700');
 })
-app.post('/login', (req, res)=>{
-  teacher = req.body.student
-  student = req.body.teacher
-  console.log(teacher, student)
-  if ((teacher && student) == ""){
-    res.render('login', {
-      error: "Type login"
-    })
-  }
-
-})
-app.get('/tech_classes', function(req, res) {
-    
-  res.sendFile(__dirname + '/templates/index.html');
-
-})
-io.on("connection", (socket)=>{
-    console.log('user connected')
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-      });
-})
-app.listen(5000)

c9920c135e7c07f1d17fd0cb6eef1c8f2ee7928a
lim5max <lionmaks2017@yandex.ru>
2019-10-26 14:10:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-var io = require('socket.io');
-var express = require('express');
+let app = require('express')();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http)
 
-var app = express()
-var io = io.listen(app);
 
-app.listen(80);
+app.get('/', function(req, res) {
+    
+    res.sendFile(__dirname + '/templates/index.html');
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
-});
+  })
+app.get('/login', function(req, res) {
+    
+  res.sendFile(__dirname + '/templates/login.html');
+
+})
+app.get('/classes', function(req, res) {
+    
+  res.sendFile(__dirname + '/templates/classes.html');
+
+})
+app.get('/tech_classes', function(req, res) {
+    
+  res.sendFile(__dirname + '/templates/index.html');
+
+})
+io.on("connection", (socket)=>{
+    console.log('user connected')
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+      });
+})
